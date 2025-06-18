500bc26063d979ffd6b55f535cc106793f90e0c4
torres-jp <torres.jpw@gmail.com>
2025-06-17 21:41:30 -0300

    sessios add

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,5 +1,35 @@
+import prisma from '@/libs/prisma'
 import { NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]/route'
 
 export async function POST(req: Request) {
-  return NextResponse.json({ message: 'recived..' })
+  const data = await req.json()
+
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json(
+      { message: 'Unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+
+  const newProject = await prisma.project.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      user: {
+        connect: {
+          id: parseInt(session?.user.id),
+        },
+      },
+    },
+  })
+
+  return NextResponse.json(newProject, {
+    status: 201,
+  })
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ function HomePage() {
 
 export default HomePage
 
-//02:00:00
+//02:24:00
 //https://www.youtube.com/watch?v=qOAUm_rAE-E&t=5272s
