fc5d7227843976dde1055cd713bce93448948b77
torres-jp <torres.jpw@gmail.com>
2025-06-21 13:49:16 -0300

    feat: implement HomePage layout with header and login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+import { Container } from '@radix-ui/themes'
+import { Metadata } from 'next'
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'Home Page',
+  description: 'This is the home page of our application.',
+}
+
 function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <Container>
+      <header className='my-4 p-10 rounded-md bg-slate-900'>
+        <h1 className='my-10 text-7xl'>NextAuth Radix</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iste
+          praesentium placeat numquam nobis alias eaque enim quisquam sint
+          ipsum, eius corrupti voluptates ut eveniet tempora ratione odit
+          pariatur veritatis? Aliquid illum tenetur, libero nulla, blanditiis,
+          laborum quis nam velit excepturi commodi deleniti sunt totam aperiam
+          omnis possimus eligendi debitis!
+        </p>
+
+        <div className='mt-5'>
+          <Link
+            href='/auth/login'
+            className='text-white bg-blue-500 px-2 py-1 rounded-md  hover:bg-blue-400'
+          >
+            Ingresa
+          </Link>
+        </div>
+      </header>
+    </Container>
+  )
 }
 
 export default HomePage
